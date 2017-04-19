400bedfabc4f941533e96a8a050c442a84d30c43
htlovo <htlbvd@gmail.com>
2017-04-19 19:13:41 +0800

    infomation...

--- gobang.js
+++ gobang.js
@@ -1,10 +1,10 @@
 	var board = document.getElementById('board');
 	var player= document.getElementById('player');
-	var playerTxt= player.firstChild;
 	var start= document.getElementById('start');
 	var startTxt=start.firstChild;
 	var tipTxt=document.getElementById('tip').firstChild;
 	var ret=document.getElementById('retract');
+	// var rep=document.getElementById('report').getElementsByTagName('p')[0];
 	var i;
 	var record;
 	//黑先
@@ -12,12 +12,14 @@
 	//定义战况
 	var situation=[];
 	//胜利条件
-	var win=/(1){5,}|1(.{14}1){4,}|1(.{15}1){4,}|1(.{16}1){4,}|0{5}|0(.{14}0){4,}|0(.{15}0){4,}|0(.{16}0){4,}/;
-	EventUtil.addHandler(start,"click",startGame);
+	var win=/(1){5,}|1(.{14}1){4,}|1(.{15}1){4,}|1(.{16}1){4,}|0{5,}|0(.{14}0){4,}|0(.{15}0){4,}|0(.{16}0){4,}/;
 	startGame();
 	//棋局开始
 	function startGame() {
 		EventUtil.removeHandler(start,"click",startGame);
+		addClass(start,"cantclick");
+		EventUtil.removeHandler(ret,"click",retract);
+		addClass(ret,"cantclick");
 		//清空棋子背景
 		var square = board.getElementsByTagName('div');
 		for (i = 0; i < square.length; i++) {
@@ -36,13 +38,10 @@
 		}
 		board.className=" blackpointer";
 		//增加棋子点击事件
-		EventUtil.addHandler(board,"click",go);
-		EventUtil.addHandler(ret,"click",retract);
+		EventUtil.addHandler(board,"mousedown",go);
 		//重置提示信息
-		playerTxt.nodeValue="黑方";
-		player.parentNode.firstChild.nodeValue="轮到";
+		player.className="blackplayer";
 		tipTxt.nodeValue="对弈开始";
-		startTxt.nodeValue="已经开始";
 		//重置先手，战况
 		piece=0;
 		for (i = 0; i < 240; i++) {
@@ -58,12 +57,13 @@
 		var target = EventUtil.getTarget(event);
 		//判断点击处是否无子
 		if (target.className.search(/whitepiece|blackpiece/)===-1) {
-			if (startTxt.nodeValue!=="重新开始") {
-				tipTxt.nodeValue="对弈中..";
-				startTxt.nodeValue="重新开始";
+			if (i==0) {
+				tipTxt.nodeValue="对弈中";
 				EventUtil.addHandler(start,"click",startGame);
+				start.className=" ";
+				EventUtil.addHandler(ret,"click",retract);
+				ret.className=" ";
 			}
-
 			//匹配win的字符串
 			var result;
 			var idNum=+target.id.replace("sq","");
@@ -77,77 +77,124 @@
 				row:Math.ceil((idNum+1)/16),
 				col:((idNum+1)%16)
 			}
-			if (i%2===0) {
-			console.log("白"+record[i-1].row+" "+record[i-1].col)
-			} else {
-			console.log("黑"+record[i-1].row+" "+record[i-1].col)
-			}
+			if (record[i-2]) {
+				record[i-2].sq.className=record[i-2].sq.className.replace("lastpiece","");
+			} 			
+			addClass(target,"lastpiece");
+			// if (i%2===0) {
+			// 	rep.innerText="白"+record[i-1].row+" "+record[i-1].col;
+			// 	console.log("白"+record[i-1].row+" "+record[i-1].col)
+			// } else {
+			// 	rep.innerText="黑"+record[i-1].row+" "+record[i-1].col;
+			// 	console.log("黑"+record[i-1].row+" "+record[i-1].col)
+			// }
 			//改变战况
 			situation[idNum] = piece;
 			if (piece) {
 				addClass(target,"whitepiece");
 				piece=0;
-				playerTxt.nodeValue="黑方";
+				player.className="blackplayer";
 				board.className=" blackpointer";
 			} else {
 				addClass(target,"blackpiece");
 				piece=1;
-				playerTxt.nodeValue="白方";
+				player.className="whiteplayer"
 				board.className=" whitepointer"
 			}
 			//战况转为字符串
 			situation=situation.join("");
 			//判断是否分出胜负
 			if (win.test(situation)) {
-				board.className="";
-				tipTxt.nodeValue="对弈结束";
-				EventUtil.removeHandler(board,"click",go);
+				board.className=" ";
+				player.className=" ";
+				EventUtil.removeHandler(board,"mousedown",go);
 				EventUtil.removeHandler(ret,"click",retract);
-				player.parentNode.firstChild.nodeValue="恭喜";
+				addClass(ret,"cantclick");
 				result=win.exec(situation);
 				if (+result[0][0]) {
-					playerTxt.nodeValue="白胜";
-					//划线
+					tipTxt.nodeValue="白胜";
+					//划线,合并部分case
 					switch(result[0].length){
-						case 5: for ( i = 0; i < 5; i++) {
+						case 5: 
+						case 6:
+						case 7:
+						case 8:
+						case 9: 
+							for ( i = 0; i < result[0].length; i++) {
 							winpiece = document.getElementById("sq"+(result.index+i));
 							addClass(winpiece,"wleftline");
 						};break;
-						case 61: for ( i = 0; i < 5; i++) {
+						case 61: 
+						case 76:
+						case 91:
+						case 106:
+						case 121:
+							for ( i = 0; i < (result[0].length-1)/15+1; i++) {
 							winpiece = document.getElementById("sq"+(result.index+i*15));
 							addClass(winpiece,"wtoprightline");
 						};break;
-						case 65: for ( i = 0; i < 5; i++) {
+						case 65:
+						case 81:
+						case 97:
+						case 113:
+						case 129:
+							for ( i = 0; i < (result[0].length-1)/16+1; i++) {
 							winpiece = document.getElementById("sq"+(result.index+i*16));
 							addClass(winpiece,"wtopline");
 						};break;
-						case 69: for ( i = 0; i < 5; i++) {
+						case 69:
+						case 86:
+						case 103:
+						case 120:
+						case 137:
+							for ( i = 0; i < (result[0].length-1)/17+1; i++) {
 							winpiece = document.getElementById("sq"+(result.index+i*17));
 							addClass(winpiece,"wtopleftline");
 						};break;
 						default: alert("!!!!!");
 					}
 				} else {
-					playerTxt.nodeValue="黑胜";
+					tipTxt.nodeValue="黑胜";
 					//划线
 					switch(result[0].length){
-						case 5: for ( i = 0; i < 5; i++) {
+						case 5: 
+						case 6:
+						case 7:
+						case 8:
+						case 9:
+							for ( i = 0; i < result[0].length; i++) {
 							winpiece = document.getElementById("sq"+(result.index+i));
 							addClass(winpiece,"bleftline");
 						};break;
-						case 61: for ( i = 0; i < 5; i++) {
+						case 61: 
+						case 76:
+						case 91:
+						case 106:
+						case 121:
+							for ( i = 0; i < (result[0].length-1)/15+1; i++) {
 							winpiece = document.getElementById("sq"+(result.index+i*15));
 							addClass(winpiece,"btoprightline");
 						};break;
-						case 65: for ( i = 0; i < 5; i++) {
+						case 65:
+						case 81:
+						case 97:
+						case 113:
+						case 129:
+							for ( i = 0; i < (result[0].length-1)/16+1; i++) {
 							winpiece = document.getElementById("sq"+(result.index+i*16));
 							addClass(winpiece,"btopline");
 						};break;
-						case 69: for ( i = 0; i < 5; i++) {
+						case 69:
+						case 86:
+						case 103:
+						case 120:
+						case 137:
+							for ( i = 0; i < (result[0].length-1)/17+1; i++) {
 							winpiece = document.getElementById("sq"+(result.index+i*17));
 							addClass(winpiece,"btopleftline");
 						};break;
 						default: alert("!!!!!");
+						console.log(result[0].length);
 					}
 				}
 			}
@@ -171,17 +218,24 @@
 			}
 			if (piece) {
 				piece=0;
-				playerTxt.nodeValue="黑方";
+				player.className="blackplayer"
 			} else {
 				piece=1;
-				playerTxt.nodeValue="白方";
+				player.className="whiteplayer"
 			}
-			if (i%2===0) {
-				console.log("-白"+record[i-1].row+" "+record[i-1].col)
-			} else {
-				console.log("-黑"+record[i-1].row+" "+record[i-1].col)
+			// if (i%2===0) {
+			// 	console.log("-白"+record[i-1].row+" "+record[i-1].col)
+			// } else {
+			// 	console.log("-黑"+record[i-1].row+" "+record[i-1].col)
+			// }
+			if (i===1) {
+				EventUtil.removeHandler(ret,"click",retract);
+				addClass(ret,"cantclick");
 			}
 			situation[record[i-1].idNum] = 2;
 			i=i-1;
+			if (record[i-1]) {
+				addClass(record[i-1].sq,"lastpiece");
+			} 
 		}
 	}
